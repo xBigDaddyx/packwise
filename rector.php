f0099b46d585bfe80067cb9402285e9733f241de
Pushpak Chhajed <pushpak1300@gmail.com>
2024-12-04 23:15:44 +0530

    style: add laravel collction sets for additional code formatting

--- rector.php
+++ rector.php
@@ -34,4 +34,5 @@ return RectorConfig::configure()
         LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
         LaravelSetList::LARAVEL_IF_HELPERS,
         LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_COLLECTION,
     ]);
