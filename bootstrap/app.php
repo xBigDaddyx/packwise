8b7477ea38e7dddde796dcf530040f7f0b093a0d
Pushpak Chhajed <pushpak1300@gmail.com>
2024-12-09 17:12:52 +0530

    feat: integrate sentry with application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions); // Intgrate Sentry with Application
     })->create();
