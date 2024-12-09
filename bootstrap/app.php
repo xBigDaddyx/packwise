0e9eb7b3dfb8e63476e2e9c9061b038b205c102e
Pushpak Chhajed <pushpak1300@gmail.com>
2024-12-09 22:54:08 +0530

    fix: linting changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,12 +8,11 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
